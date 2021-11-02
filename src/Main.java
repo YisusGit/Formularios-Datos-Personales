6d6a3a43d6c3a38bf0a2a591b6293b99c385d7b4
Jesús <jesusrr2000@gmail.com>
2021-11-02 15:05:07 +0100

    Añadimos la ventana principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main(){
+        initPantalla();
+    }
+
+    //Creamos la ventana
+    private void initPantalla() {
+        setLayout(null); //Layout absoluto
+        setTitle("Formulario"); //Título del JFrame
+        setSize(520, 520); //Dimensiones del JFrame
+        setResizable(false); //No redimensionable
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar proceso al cerrar ventana
+        setVisible(true); //Mostrar JFrame
+    }
 
     public static void main(String[] args) {
-        
+        new Main();
     }
 
 }
